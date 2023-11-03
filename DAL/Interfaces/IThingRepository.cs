d6d8eb62a3ad426afb22d231e6658fcbad36b16e
(danil) <(abc@gmail.com)>
2023-11-03 21:51:38 +0300

    Add things

---
+++ DAL/Interfaces/IThingRepository.cs
@@ -0,0 +1,8 @@
+﻿using LostButFound.API.Domian;
+
+namespace LostButFound.API.DAL.Interfaces
+{
+    public interface IThingRepository : IBaseRepository<Thing>
+    {
+    }
+}
---
+++ DAL/Repositories/ThingRepository.cs
@@ -0,0 +1,41 @@
+﻿using LostButFound.API.Domian;
+using Microsoft.EntityFrameworkCore;
+
+namespace LostButFound.API.DAL.Repositories
+{
+    public class ThingRepository
+    {
+        public ApplicationDbContext _db;
+
+        public ThingRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<bool> Create(Thing entity)
+        {
+            _db.Things.Add(entity);
+            _db.SaveChangesAsync();
+
+            return true;
+        }
+
+        public async Task<bool> Delete(Thing entity)
+        {
+            _db.Things.Remove(entity);
+            _db.SaveChangesAsync();
+
+            return true;
+        }
+
+        public async Task<Thing> Get(int id)
+        {
+            return await _db.Things.FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+        public async Task<List<Thing>> Select()
+        {
+            return await _db.Things.ToListAsync();
+        }
+    }
+}
